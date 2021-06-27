654215c0bd3c9dc4215ff7ef69f0e7853cef6d00
jimmianh <73330878+jimmianh@users.noreply.github.com>
2021-06-28 00:44:24 +0700

    lap8

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var animalGeneric = new AnimalGeneric<Animal>();
+            animalGeneric.Add(new Cat
+            {
+                Id = 12,
+                Name = "niu",
+                Dob = "hello"
+            });
+            
+            animalGeneric.Add(new Cat
+            {
+                Id = 12,
+                Name = "niu",
+                Dob = "hello"
+            });
+            animalGeneric.Add(new Cat
+            {
+                Id = 12,
+                Name = "niu",
+                Dob = "hello"
+            });
+            animalGeneric.Add(new Dog()
+            {
+                Id = 12,
+                Name = "lag",
+                Dob = "ga"
+            });
+            
+            animalGeneric.ShowInformation();
+
+        }
+    }
+}
